b6544befd773e26b3a239aef75aa8b80d37e29eb
definoob <noahjun511@gmail.com>
2022-05-30 00:00:40 -0300

    No.16

---
+++ 16.3-sum-closest.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=16 lang=typescript
+ *
+ * [16] 3Sum Closest
+ */
+
+// @lc code=start
+function threeSumClosest(nums: number[], target: number): number {
+  nums.sort((a, b) => a - b);
+  let closest = Infinity;
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      let localSum = nums[i] + nums[left] + nums[right];
+      if (Math.abs(localSum - target) < Math.abs(closest - target))
+        closest = localSum;
+      if (localSum > target) right--;
+      else left++;
+    }
+  }
+  return closest;
+}
+// @lc code=end
